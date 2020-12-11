0fd28f978add3c9660d1e64be5687f73cf5c1193
WaiYan MoeMyint <68596879+waiyanmoemyint-se@users.noreply.github.com>
2020-12-11 19:14:24 +0630

    Create Vehical.py
    Exercise Question 1: Create a Vehicle class with max_speed and mileage instance attributes

---
+++ Vehical.py
@@ -0,0 +1,8 @@
+class Vehical:
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage   = mileage
+
+final = Vehical(40,18)
+print(final.max_speed)
+print(final.mileage)
