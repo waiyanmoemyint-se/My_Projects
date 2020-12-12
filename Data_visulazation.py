49cf1180ebf033dd4a7fd8c82a8bae40de1d06e6
WaiYan MoeMyint <68596879+waiyanmoemyint-se@users.noreply.github.com>
2020-12-12 19:09:44 +0630

    Create Data_visulazation.py

---
+++ Data_visulazation.py
@@ -0,0 +1,11 @@
+#Data Visulazatoin
+
+
+from matplotlib  import pyplot
+x = [1,2,3]
+y = [4,5,6]
+pyplot.plot(x,y)
+pyplot.title("My Line Graph") 
+pyplot.xlabel("X axis")
+pyplot.ylabel("Y axis")
+pyplot.show()
