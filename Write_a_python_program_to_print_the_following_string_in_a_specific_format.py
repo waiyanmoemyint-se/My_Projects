8b27471ab4135c809cb18b591e29827d36e9b135
WaiYan MoeMyint <68596879+waiyanmoemyint-se@users.noreply.github.com>
2020-12-15 20:05:06 +0630

    Create Write_a_python_program_to_print_the_following_string_in_a_specific_format.py

---
+++ Write_a_python_program_to_print_the_following_string_in_a_specific_format.py
@@ -0,0 +1,4 @@
+print("Twinkle,twinkle,little star,How I\n"
+      " wonder what you are!Up above the world so high,\n"
+      " Like a diamond in the sky.Twinkle,twinkle,little star,\n"
+      " How I wonder what you are!")
